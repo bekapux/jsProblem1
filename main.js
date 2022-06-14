138a33c10d4f5a90a5ec57344a2475c326d4df52
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-06-14 15:30:14 +0400

    problem

---
+++ main.js
@@ -0,0 +1,47 @@
+let cars = [
+    {
+        model: 'Ferrari',
+        color: "Red",
+        fuelVolumeAndConsumption: {
+            fuelVolume: 78,
+            fuelComsumption: 20.6
+        }
+    },
+    {
+        model: 'Lambo',
+        color: "Yellow",
+        fuelVolumeAndConsumption: {
+            fuelVolume: 90,
+            fuelComsumption: 26.2
+        }
+    },
+    {
+        model: 'Bugatti',
+        color: "black",
+        fuelVolumeAndConsumption: {
+            fuelVolume: 100,
+            fuelComsumption: 35.2
+        }
+    },
+    {
+        model: 'Porsche',
+        color: "White",
+        fuelVolumeAndConsumption: {
+            fuelVolume: 80,
+            fuelComsumption: 20
+        }
+    },
+    {
+        model: 'Bugatti',
+        color: "Grey",
+        fuelVolumeAndConsumption: {
+            fuelVolume: 78,
+            fuelComsumption: 27.6
+        }
+    },
+];
+
+// fuelVolume - ავზის მოცულობა
+// fuelComsumption - ხარჯი
+
+// გამოთვალე რომელი მანქანა გაივლის ყველაზე დიდ მანძილს სავსე ავზით
